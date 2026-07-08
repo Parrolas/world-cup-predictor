d58afef9f2a3e059401958b6dd48e3e2018b810c
Parrolas <joseparrolas07@gmail.com>
2026-07-09 00:17:56 +0100

    Added more data from current games

--- src/train_xg_model.py
+++ src/train_xg_model.py
@@ -34,7 +34,7 @@ y = df['is_goal']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # 5. Train the Model!
-print("Training Logistic Regression model on 2,241 shots...")
+print(f"Training Logistic Regression model on {len(df)} shots...")
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
